33e5f96818b665fe598cf4fc46e6150436cec449
chenrk <236952059@qq.com>
2012-11-18 22:10:41 +0800

    version 1
    version 1

---
+++ hello.java
@@ -0,0 +1,16 @@
+
+public class hello {
+	public static void main(String[] args)
+	{
+		int a = 8;
+		sayhello();
+	}
+	/* This function is for show the
+	 * string of hello
+	 */
+	private void sayhello()
+	{
+	System.out.println("Hello!");
+	}
+
+}
